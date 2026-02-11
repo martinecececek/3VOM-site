7016e025f831d2f616f141799e09c84453e22d78
martinecececek <104137335+martinecececek@users.noreply.github.com>
2026-02-11 16:05:47 +0100

    minor + added pujceni - prozatim

--- admin/JS/send-file.js
+++ admin/JS/send-file.js
@@ -1,4 +1,4 @@
-const PDF_WORKER_URL = "https://little-hill-bbfa.martin-jakubuv.workers.dev";
+const PDF_WORKER_URL = "https://pdf-replace-worker.martin-jakubuv.workers.dev";
 
 const pdfForm = document.getElementById("pdfForm");
 const pdfStatus = document.getElementById("pdfStatus");
@@ -7,7 +7,8 @@ const pdfBtn = document.getElementById("pdfBtn");
 pdfForm.addEventListener("submit", async (e) => {
    e.preventDefault();
 
-   const adminKey = document.getElementById("pdfAdminKey").value.trim();
+   const adminKey =
+      "bHKJKHJKJHG6Jadpiadasd14a6s5d15691ASDADASD541a5sd1a651d3a1sd65198451ASDASASDASDASDDa16jh5gk4h665161K"; // hardcoded for simplicity, ideally should be stored securely
    const input = document.getElementById("anyFile");
    const file = input.files && input.files[0];
 
--- admin/JS/send-img.js
+++ admin/JS/send-img.js
@@ -1,4 +1,4 @@
-const WORKER_URL = "https://odd-sea-2f36.martin-jakubuv.workers.dev";
+const WORKER_URL = "https://-img-replace-worker.martin-jakubuv.workers.dev";
 
 const form = document.getElementById("uploadForm");
 const statusEl = document.getElementById("uploadStatus");
@@ -8,7 +8,8 @@ form.addEventListener("submit", async (e) => {
    e.preventDefault();
 
    const caption = document.getElementById("photoTitle").value.trim();
-   const adminKey = "bHKJKHJKJHG6Jadpiadasd14a6s5d15691ASDADASD541a5sd1a651d3a1sd65198451ASDASASDASDASDDa16jh5gk4h665161K"; // hardcoded for simplicity, ideally should be stored securely
+   const adminKey =
+      "bHKJKHJKJHG6Jadpiadasd14a6s5d15691ASDADASD541a5sd1a651d3a1sd65198451ASDASASDASDASDDa16jh5gk4h665161K"; // hardcoded for simplicity, ideally should be stored securely
    const fileInput = document.getElementById("photoFile");
    const file = fileInput.files && fileInput.files[0];
 
